b5c61b4ecc053b186778c949bcf6759ae11a129d
gabriele <gabriele.manfredi@hotmail.it>
2017-04-12 21:23:50 +0200

    high order components tutorial

--- start_using_react_webapp/react_webapp/src/App.js
+++ start_using_react_webapp/react_webapp/src/App.js
@@ -1,29 +1,60 @@
 import React, { Component } from 'react';
-import Promise from 'promise';
 import './App.css';
 
-class App extends Component {
+const App = (InnerComponent) => class extends Component {
+  constructor () {
+    super();
+    this.state = {
+      count: 0,
+      style : {
+        color: 'blue'
+      }
+    };
+  }
+  update (){
+    let currCountVal = this.state.count + 1;
+    //currCountVal++;
+    let newState = {count : currCountVal, style: {color: 'red'} }
+    this.setState(newState);
+  }
+  render(){
+    return(
+      <InnerComponent 
+        update={this.update.bind(this)} 
+        {...this.state} 
+        {...this.props} >
+
+          {this.props.children}
+
+      </InnerComponent>
+    );
+  }
+}
+
+class Layout extends Component {
   render () {
     return (
       <div>
-        <Button>Button </Button>
+        <Button {...this.props}> button children</Button>
         <hr/>
-        <Label>Label </Label>
+        <Label  {...this.props}>label children</Label>
       </div>
     );
   }
 }
 
- const Button = (props) => <button>{props.children}</button>;
+const Button = (props) => <button style={props.style} onClick={props.update}>
+  {props.children} - {props.count}
+</button>;
 
- class Label extends Component {
+class Label extends Component {
   render () {
     return (
-      <label>{this.props.children} </label>
+      <label>{this.props.children} - {this.props.count}</label>
     );
   }
 }
 
+//const LabelHOC = HOC(Label);
 
-
-export default App;
+export default App(Layout);
