0df36392d21580b930e99e9f3866a12a4167ce73
gabriele <gabriele.manfredi@hotmail.it>
2017-04-12 21:47:45 +0200

    Childrens

--- start_using_react_webapp/react_webapp/src/App.js
+++ start_using_react_webapp/react_webapp/src/App.js
@@ -1,60 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const App = (InnerComponent) => class extends Component {
-  constructor () {
-    super();
-    this.state = {
-      count: 0,
-      style : {
-        color: 'blue'
-      }
-    };
-  }
-  update (){
-    let currCountVal = this.state.count + 1;
-    //currCountVal++;
-    let newState = {count : currCountVal, style: {color: 'red'} }
-    this.setState(newState);
-  }
+class App extends Component {
+
   render(){
     return(
-      <InnerComponent 
-        update={this.update.bind(this)} 
-        {...this.state} 
-        {...this.props} >
-
-          {this.props.children}
-
-      </InnerComponent>
+      <Layout>
+        <Button value="A">A</Button>
+        <Button value="B">B</Button>
+        <Button value="C">C</Button>
+      </Layout>
     );
   }
 }
 
 class Layout extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      selected: "None"
+    }
+  }
+
+  selectItem(selected){
+    this.setState({selected});
+  }
+
+
   render () {
+    let fn = child => 
+    React.cloneElement(child, {
+          onClick: this.selectItem.bind(this, child.props.value)
+        }
+      );
+
+    let items = React.Children.map(this.props.children, fn);
+
     return (
       <div>
-        <Button {...this.props}> button children</Button>
-        <hr/>
-        <Label  {...this.props}>label children</Label>
+        <h2> Selected: {this.state.selected}</h2>
+        {items}
       </div>
     );
   }
 }
 
-const Button = (props) => <button style={props.style} onClick={props.update}>
-  {props.children} - {props.count}
-</button>;
+const Button = (props) => <button {...props}>{props.children}</button>;
 
-class Label extends Component {
-  render () {
-    return (
-      <label>{this.props.children} - {this.props.count}</label>
-    );
-  }
-}
 
-//const LabelHOC = HOC(Label);
-
-export default App(Layout);
+export default App;
